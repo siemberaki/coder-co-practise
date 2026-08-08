99804b7610bf40f92c305e5a0bd1b705bcfcd3d7
Siem Beraki <siemberaki@gmail.com>
2026-08-07 20:01:34 -0700

    finishing web app

--- docker/count.py
+++ docker/count.py
@@ -1,22 +1,35 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 import redis
 
 app = Flask(__name__)
 
 redis_host = os.environ.get('REDIS_HOST', 'redis')
-redis_port = os.environ.get('REDIS_PORT', 6379)
-r = redis.Redis(host=redis_host, port=redis_port)
+redis_port = int(os.environ.get('REDIS_PORT', 6379))
+r = redis.Redis(host=redis_host, port=redis_port, db=0, decode_responses=True)
 
 @app.route('/')
 def welcome():
-    return 'Welcome to the coderco containers challenge!'
+    try:
+        visits = int(r.get('visits') or 0)
+    except redis.RedisError:
+        visits = 0
+
+    return render_template('index.html', visits=visits)
 
 @app.route('/count')
 def count():
-    count = r.incr('visits')
-    return f'This page has been visited {count} times.'
+    try:
+        visits = r.incr('visits')
+    except redis.RedisError:
+        return 'Unable to update visit count at this time.', 500
+
+    return render_template('index.html', visits=visits)
+
+@app.route('/health')
+def health():
+    return {'status': 'ok'}
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5002)
